c64cb5271a049257a450d8e800b21219c84d5175
Richard Dallman <richdallman@googlemail.com>
2017-06-06 21:29:32 +0100

    Added file for obtaining driver names
    List is drivers who are currently signed out and have not signed in yet.

--- www/js/driversOnDuty.js
+++ www/js/driversOnDuty.js
@@ -2,7 +2,7 @@ function getDriversOnDuty() {
   console.log("Get driver names signed out (1)");
   //alert("Get drivers called");
   var drivers;
-  $.getJSON("http://localhost:8080/project/test1/driverSignInNamesddl.php", function (data) {
+  $.getJSON("http://localhost:8080/project/test1/driverSignInNamesCordova.php", function (data) {
     $.each(data,function(index,item) {
           drivers+="<option value='"+item.payeNumber+"'>" + item.firstName + " " + item.lastName + " " + "(" + item.payeNumber + ")" +"</option>";
           console.log("Get driver names signed out (2)");
