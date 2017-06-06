b7296cea2a62b2d80e32928eedf92e08c68c1066
Richard Dallman <richdallman@googlemail.com>
2017-06-06 21:15:46 +0100

    Driver sign in layout created
    Finished much of the layout for the driver sign in screen.
    Still to add is the vehicle number for the selected driver.
    The driver name drop down list is not filling with the names, although
    results are returned correctly from the query.
    
    Created a JS file, driverSignOut.js, to deal with the handling of
    queries in the process of signing out.

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -28,55 +28,7 @@ var app = {
     };
 
     function project() {
-
-      //Returns a list of all those drivers who do not have a record in the dutyDetails table
-      //for the current date, that is, they have not signed out already today
-      function getDriversNotOnDuty() {
-        //alert("Get drivers called");
-        var drivers;
-        $.getJSON("http://localhost:8080/project/test1/getDriverNamesCordova.php", function (data) {
-          $.each(data,function(index,item) {
-                drivers+="<option value='"+item.payeNumber+"'>" + item.firstName + " " + item.lastName + " " + "(" + item.payeNumber + ")" +"</option>";
-                console.log("Get driver names worked");
-        });
-          $('#driverNames').html(drivers);
-      });
-    }; //end of getDriversNotOnDuty
-
-    //Returns a list of all those duties which do not have a record in the dutyDetails table
-    //for the current date, that is, they have not been signed out already today
-    function getDutiesNotOut() {
-      //alert("Get duties called");
-      var duties;
-      $.getJSON("http://localhost:8080/project/test1/dutyNumbers.php", function (data) {
-        $.each(data,function(index,item) {
-              duties+="<option value='"+item.dutyNumber+"'>" + item.dutyNumber+"</option>";
-              console.log("Get duty numbers worked");
-      });
-        $('#dutyNumber').html(duties);
-    });
-  }; //end of getDutiesNotOut
-
-    //Returns a list of all those vans which do not have a record in the dutyDetails table
-    //for the current date, that is, they have not been
-    function getVansNotOut() {
-      //alert("Get vans called");
-      var vans;
-      $.getJSON("http://localhost:8080/project/test1/getVanNumbersSignOut.php", function (data) {
-        $.each(data,function(index,item) {
-              vans+="<option value='"+item.vehicleNumber+"'>" + item.vehicleNumber+"</option>";
-              console.log("Get van numbers worked");
-      });
-        $('#vanNumber').html(vans);
-    });
-  }; //end of getVansNotOut
-
-      //Calling these functions here causes them to be called when index.html is loaded.
-      //Instead I want these functions to be called only when required.
       
-      //getDriversNotOnDuty();
-      //getDutiesNotOut();
-      //getVansNotOut();
     }; //end of project function
     this.project = new project();
 } //end of bindEvents
