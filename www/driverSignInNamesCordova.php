336de8364b99865e4ee3763417eb740b85a98c35
Richard Dallman <richdallman@googlemail.com>
2017-06-07 19:05:10 +0100

    Retrieving driver names / van numbers
    Added a js file for getting the list of drivers out on duty. This will
    be used for signing back in upon returning to the office.
    
    Retrieving the van numbers will, once I have it finished, retrieve the
    van number associated to selected staff member.

--- www/driverSignInNamesCordova.php
+++ www/driverSignInNamesCordova.php
@@ -1,11 +1,22 @@
 <?php
+include 'database.php';
 
-  $sql = "SELECT firstName, lastName, payeNumber from projectdb1.dutyDetails INNER JOIN vans ON dutydetails.vanNumber = vans.vehicleNumber
-  INNER JOIN staff ON dutydetails.staffMember = staff.payeNumber WHERE DATE(timeIn) IS NULL ORDER BY payeNumber ASC";
+  $sql = "SELECT
+    firstName, lastName, payeNumber
+FROM
+    projectdb1.dutyDetails
+        INNER JOIN
+    vans ON dutydetails.vanNumber = vans.vehicleNumber
+        INNER JOIN
+    staff ON dutydetails.staffMember = staff.payeNumber
+WHERE
+    DATE(timeIn) IS NULL
+ORDER BY payeNumber ASC";
+
   $result = $conn->query($sql);
       if($result -> num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
-          $data[] =$row;
+          $data[] = $row;
     }
     $myJSON = json_encode($data);
     echo $myJSON;
