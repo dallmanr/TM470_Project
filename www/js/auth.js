fbd677fe61dbd51699a0babf1012146cb23104ca
Richard Dallman <richdallman@googlemail.com>
2017-06-28 16:34:37 +0100

    Fixed admin back button and authing users
    The back button on the Admin control page now returns home and clears
    the admin name from local stroage.
    
    auth.js takes the value entered in a prompt dialog box and checks the
    database for a valid password. If it is found, the name of the user
    along with paye number is assigned to localstorage for use in the
    add/remove van (when implemented).

--- www/js/auth.js
+++ www/js/auth.js
@@ -4,21 +4,28 @@
 function authUser() {
   //Test the call to function works
   console.log("authUser in auth.js called");
-  var x = prompt("Please enter your password");
-  console.log(x);
+  var pw = prompt("Please enter your password");
+  console.log("Entered value " + pw);
   var firstName;
   var lastName;
   var payeNum;
   var pw;
 
-  $.getJSON("http://86.0.13.186:8080/tm470/queries/authUsers.php", function(data) {
-    var obj = $.parseJSON(data);
-    firstName = obj[0].firstName;
-    lastName = obj[0].lastName;
-    payeNumber = obj[0].payeNumber;
+  $.ajax({
+    type: "POST",
+    url: "http://86.0.13.186:8080/tm470/queries/authUsers.php",
+    data: "pw=" + pw,
+    //dataType: "jsonp",
+    success: function(data) {
+      var obj = $.parseJSON(data);
+      firstName = obj[0].firstName;
+      lastName = obj[0].lastName;
+      payeNumber = obj[0].payeNumber;
 
-    full = firstName + " " + lastName + " (" + payeNumber + ")";
-    localStorage.setItem('adminName', full);
-    console.log("Full name is " + full);
+      full = firstName + " " + lastName + " (" + payeNumber + ")";
+      localStorage.setItem('adminName', full);
+      console.log(localStorage.getItem("adminName"));
+      document.location.href = "admin/index.html";
+    }
   });
 };
